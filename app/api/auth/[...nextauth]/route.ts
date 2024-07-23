20fb5251f8ca0f6b02b2cb76eb6c353c0f7a3fc1
sugawara-kouki <kou.720131@gmail.com>
2024-07-23 23:30:54 +0900

    feat: prismaとmysqlを用いたユーザ登録機能の追加

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,13 @@
+import NextAuth, { NextAuthOptions } from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { PrismaClient } from '@prisma/client';
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import type { Adapter } from 'next-auth/adapters';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-export const authOptions = {
+export const authOptions:NextAuthOptions = {
+  adapter:PrismaAdapter(prisma) as Adapter,
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
@@ -13,6 +15,22 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET ?? '',
     }),
   ],
+  session: {
+    strategy: "database",
+    maxAge: 60 * 60 * 24 * 30,
+    updateAge: 60 * 60 * 24,
+  },
+
+  useSecureCookies: process.env.NODE_ENV === "production",
+  callbacks: {
+    async redirect({baseUrl}) {
+      return baseUrl;
+    },
+    async session({session, user}) {
+      if(session?.user) session.user.id = user.id;
+      return session;
+    }
+  }
 }
 
 const handler = NextAuth(authOptions);
